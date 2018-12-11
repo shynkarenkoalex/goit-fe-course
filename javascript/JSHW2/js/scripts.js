16fe56149df46bcb72b23212664a2c9f3629e967
shynkarenkoalex <shynkarenkoalex5@gmail.com>
2018-12-12 01:06:09 +0200

    first commit

--- javascript/JSHW2/js/scripts.js
+++ javascript/JSHW2/js/scripts.js
@@ -1,26 +1,23 @@
+'use strict';
+let userInput;
+const numbers = [];
+let total = 0;
+do{
+  userInput = Number(prompt("введите число", "")); 
+  numbers.push(userInput);
+  }while(userInput !== 0);
+numbers.pop(userInput);
+console.log("Inputed array is: " + numbers);
 
-//<script type="text/javascript">
-//function Input() {
-//const ADMIN_LOGIN = 'admin'; 
-//const ADMIN_PASSWORD = 'm4ngo1zh4ackz0r'; 
-    
-    
-login_ok = false;
-ADMIN_LOGIN = "";
-ADMIN_PASSWORD = "";
-ADMIN_LOGIN = prompt("ADMIN_LOGIN","");
-ADMIN_LOGIN = ADMIN_LOGIN.toLowerCase();
-
-ADMIN_PASSWORD = prompt("ADMIN_PASSWORD","");
-ADMIN_PASSWORD = ADMIN_PASSWORD.toLowerCase();
-if (ADMIN_LOGIN=="admin" && ADMIN_PASSWORD=="m4ngo1zh4ackz0r") {
-login_ok=true;
-
-window.location="https://github.com/shynkarenkoalex/goit-fe-course";
-alert("Добро пожаловать!");
+for(let i of numbers){
+  total += i;
 }
 
+if (numbers.length >= 1){
+  alert(`Общая сумма чисел равна ${total}`);
+} else{
+    userInput;
+}
+console.log(`Общая сумма чисел равна ${total}`);
+console.log(typeof userInput);
 
-if (login_ok==false) alert("Доступ запрещен!");
-//}
-//</script>
