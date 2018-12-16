3cc35b22eafa09cefc13870c33bc988cd909fb2a
shynkarenkoalex <shynkarenkoalex5@gmail.com>
2018-12-16 17:45:39 +0200

    first commit

--- javascript/JSHW3/js/scripts.js
+++ javascript/JSHW3/js/scripts.js
@@ -1,26 +1,56 @@
-
-//<script type="text/javascript">
-//function Input() {
-//const ADMIN_LOGIN = 'admin'; 
-//const ADMIN_PASSWORD = 'm4ngo1zh4ackz0r'; 
-    
-    
-login_ok = false;
-ADMIN_LOGIN = "";
-ADMIN_PASSWORD = "";
-ADMIN_LOGIN = prompt("ADMIN_LOGIN","");
-ADMIN_LOGIN = ADMIN_LOGIN.toLowerCase();
-
-ADMIN_PASSWORD = prompt("ADMIN_PASSWORD","");
-ADMIN_PASSWORD = ADMIN_PASSWORD.toLowerCase();
-if (ADMIN_LOGIN=="admin" && ADMIN_PASSWORD=="m4ngo1zh4ackz0r") {
-login_ok=true;
-
-window.location="https://github.com/shynkarenkoalex/goit-fe-course";
-alert("Добро пожаловать!");
-}
+'use strict';
 
 
-if (login_ok==false) alert("Доступ запрещен!");
-//}
-//</script>
+const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
+console.log('Existing logins database: ', logins);
+const updatedLoginBase = [logins];
+let login = prompt('Please, enter your login');
+
+
+const checkLoginValidity = function(login) {
+    if (4 <= login.length && login.length <= 16) {
+        return true;
+    }
+    return false;
+};
+const checkLoginValidityResult = checkLoginValidity(login);
+
+
+const checkIfLoginExists = function(logins, login) {
+    if (logins.includes(login)) {
+        return true;
+    }
+    return false;
+};
+const checkIfLoginExistsResult = checkIfLoginExists(logins, login);
+
+
+const addLogin = function(logins, login) {
+    console.log('Entered login: ', login);
+    console.log('login length:', login.length);
+    checkLoginValidity(login);
+    console.log('Length Validity (4-16):', checkLoginValidityResult);
+
+    if (!checkLoginValidityResult) {
+        return console.log('Ошибка! Логин должен быть от 4 до 16 символов');
+    } else {
+        console.log(
+            `Checking login " ${login} " for existance in the base of logins...`,
+        );
+        checkIfLoginExists(logins, login);
+    }
+    if (!checkIfLoginExistsResult) {
+        console.log('Данный логин отсутствует в базе логинов');
+        console.log('Логин успешно добавлен!');
+        logins.push(login);
+    } else {
+        console.log('Такой логин уже используется!');
+    }
+};
+
+
+addLogin(logins, login);
+
+
+
+console.log('updated base of logins: ', logins);
